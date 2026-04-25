86b1fc75a0eb86f34ff7691bbc814342a6ca04bc
Kei-Luna <touhousetsuna@gmail.com>
2026-04-25 14:18:22 +0900

    fix fix fix fix

--- Common/Data/GameData.cs
+++ Common/Data/GameData.cs
@@ -12,7 +12,7 @@ public static class GameData
 public static class GameResourceTemplateId
 {
     public static ulong FromGdpl(uint genre, uint detail, uint particular, uint level) =>
-        ((ulong)genre << 48) | ((ulong)detail << 32) | ((ulong)particular << 16) | level;
+        ((ulong)level << 48) | ((ulong)particular << 32) | ((ulong)detail << 16) | genre;
 
     public static ulong FromGdpl(IReadOnlyList<uint> gdpl) =>
         gdpl.Count >= 4 ? FromGdpl(gdpl[0], gdpl[1], gdpl[2], gdpl[3]) : 0;
